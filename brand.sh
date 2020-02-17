bc5c09e2755a4c066d842b3c47f3106d2d4e694e
Gregory Croisdale <gtcroisdale1@gmail.com>
2020-02-17 11:45:08 -0500

    Branding support

---
+++ brand.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FH=2570
+BRAND="Calisto"
+TOT=$(($FH+${#BRAND}))
+RED='\033[0;31m'
+NC='\033[0m'
+cd ascii
+for I in $(ls)
+do
+	echo $I
+	SIZE=$(wc -c < $I)
+	BEG=$(head -c $FH $I)
+	LEFT=$(($SIZE-$TOT))
+	S_LEFT=$(tail -c $LEFT $I)
+	echo -n "$BEG " > $I
+	echo -en  "$RED$BRAND$NC " >> $I
+	echo "$S_LEFT" >> $I
+done
